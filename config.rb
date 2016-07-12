c474d609ccbe98ca9eb37939fc321769dca7435f
koudaiii <cs006061@gmail.com>
2016-07-12 20:26:39 +0900

    Fix path

--- config.rb
+++ config.rb
@@ -6,5 +6,5 @@ Dotenv.load
 erb = File.open('kubernetes/create-secret.sh.erb') { |f| ERB.new(f.read) }
 File.write('kubernetes/create-secret.sh', erb.result(binding))
 
-erb = File.open('examples/quay/quay.yaml.erb') { |f| ERB.new(f.read) }
-File.write('examples/quay/quay.yaml', erb.result(binding))
+erb = File.open('kubernetes/quay.yaml.erb') { |f| ERB.new(f.read) }
+File.write('kubernetes/quay.yaml', erb.result(binding))
