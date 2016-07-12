1b2ad360fe8ae9516d732e51d8376ab9d3aaf06d
koudaiii <cs006061@gmail.com>
2016-07-12 17:22:28 +0900

    Create quay.yaml

--- config.rb
+++ config.rb
@@ -5,3 +5,6 @@ Dotenv.load
 
 erb = File.open('kubernetes/create-secret.sh.erb') { |f| ERB.new(f.read) }
 File.write('kubernetes/create-secret.sh', erb.result(binding))
+
+erb = File.open('examples/quay/quay.yaml.erb') { |f| ERB.new(f.read) }
+File.write('examples/quay/quay.yaml', erb.result(binding))
